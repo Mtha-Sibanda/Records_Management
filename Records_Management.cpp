c2d302f99a9e3093d7535a75900f4370439b9971
Mtha-Sibanda <sib20001@byui.edu>
2023-02-10 13:11:30 -0700

    Created Main Menu

--- Records_Management.cpp
+++ Records_Management.cpp
@@ -2,19 +2,25 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   while (true) {
+      cout << "\t\t\t--------------  Main Menu  --------------" << endl << endl;
+      cout << "\t\t\t\t1. Add Employee" << endl;
+      cout << "\t\t\t\t2. Edit Employee" << endl;
+      cout << "\t\t\t\t3. Delete Employee" << endl;
+      cout << "\t\t\t\t4. View Employee" << endl;
+      cout << "\t\t\t\t5. Exit" << endl << endl;
+      cout << "\t\t\t\tEnter your choice: ";
+
+      int choice;
+      cin >> choice;
+
+      if (choice == 5) {
+         break;
+      }
+
+   }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
